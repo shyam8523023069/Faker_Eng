154d43e8e9a282add69c5bb3677f29f8e149720a
root <root@saipc.localdomain>
2020-09-20 20:53:03 +0530

    data added

--- faker.js
+++ faker.js
@@ -1,10 +1,12 @@
-function fakedata(faker, randomName, randomEmail, randomCard) {
+function fakedata(faker, randomName, randomEmail, randomCard, occupation) {
     var faker = require('faker');
     var randomName = faker.name.findName(); // Rowan Nikolaus
     var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
     var randomCard = faker.helpers.createCard(); // random contact card containing many properties
+    var occupation = faker.occupation(); // tech
     console.log(randomName);
     console.log(randomEmail);
     console.log(randomCard);
+    console.log(randomoccupation)
     }
     fakedata();
