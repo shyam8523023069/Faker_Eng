1517d2415f5b10483b2fd332ec965ca5f9bf4a49
shweta <shwetaramgupta95@gmail.com>
2020-09-25 18:47:44 +0530

    added new category called agriculture

--- examples/node/minimal-usage.js
+++ examples/node/minimal-usage.js
@@ -5,13 +5,16 @@ faker.locale = "en";
 
 //console.log(faker.lorem.sentences())
 
-console.log(faker.name.findName())
+//console.log(faker.name.findName())
+console.log(faker.agriculture.grains())
+console.log(faker.agriculture.vegetables())
+console.log(faker.agriculture.dryfruits())
 return;
 //console.log(faker.address)
-console.log(faker.internet.email())
-console.log(faker.date.recent())
-console.log(faker.helpers.contextualCard());
+//console.log(faker.internet.email())
+//console.log(faker.date.recent())
+//console.log(faker.helpers.contextualCard());
 
-faker.locale = "uk";
+//faker.locale = "uk";
 
 console.log(faker.helpers.contextualCard());
